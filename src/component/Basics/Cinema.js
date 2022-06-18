633d0638e17f6d15b537a9de731ea18aeb6b06fb
Elevatedpeace <Peace08182443958>
2022-06-18 20:40:10 +0100

    finally got it, just have to implement a local storage next

--- src/component/Basics/Cinema.js
+++ src/component/Basics/Cinema.js
@@ -1,24 +1,86 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css";
 import Movie from "./movieApi";
-import MovieCard from "./movieCard";
+import MovieCard from "./Card";
+import Card from "./Card";
 import Navbar from "./Navbar";
-const uniqueList = [
-  ...new Set(
-    Movie.map((currEle) => {
-      return currEle;
-    })
-  ),
-];
 
 const Cinema = () => {
-  const [menuList, setMenuList] = React.useState(uniqueList);
-  console.log(menuList);
-  menuList.sort((e,b) => e.likes > b.likes ? console.log(e): console.log(b));
+  const [menuList, setMenuList] = React.useState(Movie);
+
+  const [displayLike, setDisplayLike] = useState([
+    { id: 1, like: "like", state: false },
+    { id: 2, like: "like", state: false },
+    { id: 3, like: "like", state: false },
+    { id: 4, like: "like", state: false },
+    { id: 5, like: "like", state: false },
+    { id: 6, like: "like", state: false },
+  ]);
+
+  //handles the like functions
+  const addLike = (id) => {
+    setDisplayLike((prevLike) =>
+      prevLike.map((lik) =>
+        lik.id === id ? { ...lik, like: "Unlike", state: true } : lik
+      )
+    );
+
+    setMenuList((prevMovie) =>
+      prevMovie.map((item) =>
+        item.id === id ? { ...item, likes: item.likes + 1 } : item
+      )
+    );
+  };
+  const subtractLike = (id) => {
+    setDisplayLike((prevLike) =>
+      prevLike.map((lik) =>
+        lik.id === id ? { ...lik, like: "like", state: false } : lik
+      )
+    );
+    setMenuList((prevMovie) =>
+      prevMovie.map((item) =>
+        item.id === id ? { ...item, likes: item.likes - 1 } : item
+      )
+    );
+  };
+  const handleLike = (id) => {
+    const content = displayLike.find((item) => item.id === id);
+    console.log(content.state);
+    if (content.state === false) {
+      addLike(id);
+    } else {
+      subtractLike(id);
+    }
+  };
+
+  function compare_like(a, b) {
+    // a should come before b in the sorted order
+    if (a.likes > b.likes) {
+      return -1;
+      // a should come after b in the sorted order
+    } else if (a.likes < b.likes) {
+      return 1;
+      // a and b are the same
+    } else {
+      return 0;
+    }
+  }
+
   return (
     <div>
       <Navbar />
-      <MovieCard menuD={menuList} />
+      <section className="main-card--cointainer">
+        {menuList.sort(compare_like).map((item) => {
+          return (
+            <Card
+              key={item.id}
+              item={item}
+              handleLike={handleLike}
+              displayLike={displayLike}
+            />
+          );
+        })}
+      </section>
     </div>
   );
 };
