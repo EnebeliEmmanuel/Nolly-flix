ed10c7cc8543587936c724263cfab29a4c6cc6e0
KcPele <fidekg123@gmail.com>
2022-06-18 19:05:28 +0100

    added likes sorting

--- src/component/Basics/Cinema.js
+++ src/component/Basics/Cinema.js
@@ -1,24 +1,55 @@
-import React from "react";
+import React, {useState } from "react";
 import "./style.css";
 import Movie from "./movieApi";
 import MovieCard from "./movieCard";
+import Card from "./Card";
 import Navbar from "./Navbar";
-const uniqueList = [
-  ...new Set(
-    Movie.map((currEle) => {
-      return currEle;
-    })
-  ),
-];
 
 const Cinema = () => {
-  const [menuList, setMenuList] = React.useState(uniqueList);
-  console.log(menuList);
-  menuList.sort((e,b) => e.likes > b.likes ? console.log(e): console.log(b));
+  const [menuList, setMenuList] = React.useState(Movie);
+  const [toggleLike, setToggleLike] = useState(false);
+
+  const handleLike = (id) => {
+    if(toggleLike) {
+      setMenuList(prevMovie =>  prevMovie.map(item => 
+        item.id === id ?
+        {...item, likes: item.likes - 1}
+        : item))
+        setToggleLike(false)
+    } else {
+      setMenuList(prevMovie =>  prevMovie.map(item => 
+        item.id === id ?
+        {...item, likes: item.likes + 1}
+        : item))
+        setToggleLike(true)
+
+    }
+      
+  }
+
+  function compare_like(a, b){
+    // a should come before b in the sorted order
+    if(a.likes > b.likes){
+            return -1;
+    // a should come after b in the sorted order
+    }else if(a.likes < b.likes){
+            return 1;
+    // a and b are the same
+    }else{
+            return 0;
+    }
+}
+  
   return (
     <div>
       <Navbar />
-      <MovieCard menuD={menuList} />
+      <section className="main-card--cointainer">
+      {menuList.sort(compare_like).map((item) => {
+        return<Card key={item.id} item={item} handleLike={handleLike} toggleLike={toggleLike}/>
+      }
+        
+      )}
+      </section>
     </div>
   );
 };
